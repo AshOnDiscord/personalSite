82dd118196568aa9669eb2b200d215ebd887fc1c
AshOnDiscord <68016221+AshOnDiscord@users.noreply.github.com>
2025-01-21 08:55:51 -0500

    config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+
+  // <meta name="darkreader-lock">
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      meta: [{ name: "darkreader-lock", content: "" }],
+    },
+  },
+
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint", "@nuxt/content"],
+
+  typescript: {
+    typeCheck: true,
+  },
+});
