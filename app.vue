1bd0920145e31b14618b858b8c0b73993421daab
AshOnDiscord <68016221+AshOnDiscord@users.noreply.github.com>
2025-01-21 02:15:45 -0500

    reset

--- app.vue
+++ app.vue
@@ -1,18 +1,6 @@
 <template>
   <div>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
   </div>
 </template>
-
-<style>
-.page-enter-active,
-.page-leave-active {
-  transition: all 0.4s ease;
-}
-.page-enter-from,
-.page-leave-to {
-  opacity: 0;
-}
-</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,49 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
-  devtools: { enabled: true },
-
-  typescript: {
-    // typeCheck: true,
-  },
-
-  nitro: {
-    experimental: {
-      websocket: true,
-      database: true,
-    },
-  },
-
-  app: {
-    pageTransition: { name: "page", mode: "out-in" },
-  },
-
-  modules: ["@nuxt/eslint", "@nuxt/content", "@nuxt/ui", "@nuxt/image"],
-
-  content: {
-    highlight: {
-      // theme: {
-      //   default: "catppuccin-latte",
-      //   dark: "catppuccin-macchiato",
-      // },
-      theme: "catppuccin-latte",
-    },
-  },
-
-  tailwindcss: {
-    cssPath: ["~/assets/css/tailwind.css", { injectPosition: "first" }],
-    configPath: "tailwind.config",
-    exposeConfig: {
-      level: 2,
-    },
-    config: {},
-    viewer: true,
-    editorSupport: true,
-  },
-
-  eslint: {
-    config: {
-      stylistic: false,
-    },
-  },
-});
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true }
+})
